b6b5cdd7d964834c0a34b2e1abac178db4213f0f
Júlio Cézar <captaingamergroot@gmail.com>
2023-01-10 21:08:03 -0300

    Create Summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
-  background: ${(props) => props.theme.colors["gray-900"]};
+  background: ${(props) => props.theme["gray-900"]};
   padding: 2.5rem 0 7.5rem;
 `;
 
@@ -9,6 +9,7 @@ export const HeaderContent = styled.div`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
+  padding: 0 1.5rem;
 
   display: flex;
   justify-content: space-between;
@@ -18,15 +19,15 @@ export const HeaderContent = styled.div`
 export const NewTransactionButton = styled.button`
   height: 50px;
   border: 0;
-  background: ${(props) => props.theme.colors["green-500"]};
-  color: ${(props) => props.theme.colors.white};
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme.white};
   font-weight: bold;
   padding: 0 1.25rem;
   border-radius: 6px;
   cursor: pointer;
 
   &:hover {
-    background: ${(props) => props.theme.colors["green-700"]};
+    background: ${(props) => props.theme["green-700"]};
     transition: background-color 0.2s;
   }
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,18 +10,17 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus{
       outline: 0;
-      box-shadow:  0 0 0 2px ${(props) => props.theme.colors["green-500"]}
+      box-shadow:  0 0 0 2px ${(props) => props.theme["green-500"]}
     }
 
     body{
-        background: ${(props) => props.theme.colors["gray-800"]};
-        color: ${(props) => props.theme.colors["gray-100"]};
+        background: ${(props) => props.theme["gray-800"]};
+        color: ${(props) => props.theme["gray-100"]};
         -webkit-font-smoothing: antialiased;
     }
 
     body,input,textarea, button{
-        font: 400 ${(props) =>
-          props.theme.textSizes["text-regular-m"]} Roboto, sans-serif;
+        font: 400 1rem Roboto, sans-serif;
     }
 
     button{
